6ca926fb8fcc3de262d911227e12ba67bfdfb0e0
Geetha <geethasyamakula@gmail.com>
2023-09-30 15:02:08 -0500

    changed image input length

--- django_project/models.py
+++ django_project/models.py
@@ -6,4 +6,4 @@ class Stuff(models.Model):
     manufacturer = models.CharField(max_length=10)
     cost = models.IntegerField(max_length=10)
     weight = models.IntegerField(max_length=10)
-    image = models.CharField(max_length=30)
+    image = models.CharField(max_length=256)
